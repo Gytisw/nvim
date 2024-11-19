8dd044d0458d8e80af974aef3893ee2628865283
Gytisw <Gytisw>
2024-11-19 15:41:09 +0100

    Updated config files

---
+++ init.lua
@@ -0,0 +1,3 @@
+os.execute("export PATH=$PATH:/data/data/com.termux/files/home/.local/share/nvim/mason/bin/")
+require("config.options")
+require("config.lazy")
---
+++ test.py
@@ -0,0 +1,6 @@
+lol = ["fail", "blob"]
+
+
+def kool(x, y):
+    if x != y:
+        return print(x, y)
